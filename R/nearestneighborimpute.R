ece2a80e405d5c73203d27c86fd00c64c5ef1962
José Gerardo Tamez-Peña <50146167+joseTamezPena@users.noreply.github.com>
2020-08-01 20:30:42 -0500

    Data Imputation for ordered data sets

--- R/nearestneighborimpute.R
+++ R/nearestneighborimpute.R
@@ -1,4 +1,4 @@
-nearestNeighborImpute <- function(tobeimputed,referenceSet=NULL,catgoricCol=NULL,distol=1.05)
+nearestNeighborImpute <- function(tobeimputed,referenceSet=NULL,catgoricCol=NULL,distol=1.05,useorder=TRUE)
 {	
 	if (is.null(referenceSet))
 	{
@@ -18,53 +18,82 @@ nearestNeighborImpute <- function(tobeimputed,referenceSet=NULL,catgoricCol=NULL
 	IQRvalues[IQRvalues==0] <- 1;
 	if (!is.null(catgoricCol))
 	{
-		IQRvalues[colnames(tobeimputed) %in% catgoricCol] <- 0.00001;
+		IQRvalues[colnames(tobeimputed) %in% catgoricCol] <- 0.01;
+		catvalues <- imputeddata[1,catgoricCol]
 	}
+	else
+	{
+		useorder <- FALSE
+	}
+#	print(IQRvalues)
+	
 	for (i in 1:nrow(imputeddata))
 	{
+#		print(rownames(imputeddata)[i]);
 		nacol <- is.na(imputeddata[i,]);
 		if ((i %% 10)==0) cat(".");
 		if ((i %% 500)==0) cat(i,"\n");
 		if (any(nacol))
 		{	
-			if (sum(1*(!nacol)) == 0)
+			if (useorder)
 			{
-				imputeddata[i,] <- medianvalues;
-			}
-			else
-			{
-				redtrain <- trainset[,!nacol];
-				datatrain <- as.data.frame(trainset[,nacol]);
-				theCompleteCases <- complete.cases(datatrain)
-				datatrain <- as.data.frame(datatrain[theCompleteCases,])
-				redtrain <- redtrain[theCompleteCases,]
-				redimputed <- as.numeric(imputeddata[i,!nacol]);
-				distance <- abs(sweep(redtrain,2,redimputed,"-"))
-				distance <- sweep(distance,2,IQRvalues[!nacol],"/");
-				
-				if (sum(!nacol) > 1)
+				if (sum(imputeddata[i,catgoricCol] == catvalues) == 0)
 				{
-					distance <- apply(distance,1,mean, na.rm = TRUE);
+					if (i>1)
+					{
+						imputeddata[i,nacol] <- imputeddata[i-1,nacol];
+					}
 				}
-				distance <- as.numeric(distance);
-				mindistance <- distol*min(distance);
-				wsmaller <- (distance<=mindistance);
-				utrainset <- datatrain[wsmaller,];
-				if (sum(wsmaller)==1)
+				catvalues <- imputeddata[i,catgoricCol]
+				nacol <- is.na(imputeddata[i,]);
+			}
+			if (any(nacol))
+			{
+				if (sum(1*(!nacol)) == 0)
 				{
-					imputeddata[i,nacol] <- as.numeric(utrainset);
+					imputeddata[i,] <- medianvalues;
 				}
 				else
 				{
-					if (sum(nacol) > 1)
+					redtrain <- trainset[,!nacol];
+					datatrain <- as.data.frame(trainset[,nacol]);
+					theCompleteCases <- complete.cases(redtrain)
+					datatrain <- as.data.frame(datatrain[theCompleteCases,])
+					redtrain <- redtrain[theCompleteCases,]
+					redimputed <- as.numeric(imputeddata[i,!nacol]);
+					distance <- abs(sweep(redtrain,2,redimputed,"-"))
+					if (!is.null(catgoricCol))
 					{
-						mv <- as.numeric(apply(utrainset,2,median, na.rm = TRUE));
+						colnames(distance) <- colnames(redtrain)
+						distance[,catgoricCol] <- 1.0*(distance[,catgoricCol] > 0)
+					}
+					distance <- sweep(distance,2,IQRvalues[!nacol],"/");
+					
+					if (sum(!nacol) > 1)
+					{
+						distance <- apply(distance,1,mean, na.rm = TRUE);
+					}
+					distance <- as.numeric(distance);
+					mindistance <- distol*min(distance);
+					wsmaller <- (distance<=mindistance);
+					utrainset <- datatrain[wsmaller,];
+
+					if (sum(wsmaller)==1)
+					{
+						imputeddata[i,nacol] <- as.numeric(utrainset);
 					}
 					else
 					{
-						mv <- as.numeric(median(utrainset,na.rm = TRUE));
+						if (sum(nacol) > 1)
+						{
+							mv <- as.numeric(apply(utrainset,2,median, na.rm = TRUE));
+						}
+						else
+						{
+							mv <- as.numeric(median(utrainset,na.rm = TRUE));
+						}
+						imputeddata[i,nacol] <- mv;
 					}
-					imputeddata[i,nacol] <- mv;
 				}
 			}
 		}
